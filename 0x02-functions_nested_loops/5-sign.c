91e5331a67ab246e0516035eb002ca985488e7dc
moe <amohamedmahrez@gmail.com>
2020-02-12 10:57:40 +0100

    still on it

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
 * print_sign - check the code for Holberton School students.
-* @n : variable
+* @b : variable
 * Return: Always 0.
 */
-int print_sign(int n)
+int print_sign(int b)
 {
-if (n > 0)
+if (b > 0)
 {
 _putchar('+');
 return (1);
 }
-else if (n == 0)
+else if (b == 0)
 {
 _putchar('0');
 return (0);
