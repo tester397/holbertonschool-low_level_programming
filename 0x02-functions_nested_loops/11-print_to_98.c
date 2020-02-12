1349c9774e2efb713c19e5012e563a84589ed673
moe <amohamedmahrez@gmail.com>
2020-02-12 11:39:21 +0100

    mandatory done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,31 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_to_98 - function natural
- * @a: print to 98
- * Return: n
- */
-void print_to_98(int a)
+* print_to_98 - check the code for Holberton School students.
+* @n : variable
+* Return: Always 0.
+*/
+void print_to_98(int n)
 {
 int i;
-
-if (a < 98)
+if (n < 98)
 {
-for (i = a; i <= 98; i++)
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
 if (i != 98)
-printf("%i, ", i);
-else
-printf("%i\a", i);
+printf(", ");
+}
+printf("\n");
 }
 else
 {
-for (i = a; i >= 98; i--)
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
 if (i != 98)
-printf("%i, ", i);
-else
-printf("%i\a", i);
+printf(", ");
+}
+printf("\n");
 }
 }
